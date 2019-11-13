27c158ced7a0f40b373cba7e607c317bc6610deb
Keyi Zhong <kz25@uw.edu>
2019-11-12 19:02:55 -0800

    read first article of MT

--- scrap.py
+++ scrap.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import re
-import random
+import os
+
 
 
 search_url = "https://www.gutenberg.org/ebooks/authors/search/?query="
@@ -20,9 +21,15 @@ def mainloop():
 
     html = urlopen(base_url + book_url).read().decode('utf-8')
     soup = BeautifulSoup(html, features='lxml')
+    name = soup.find('h1').contents[0]
     download = soup.find_all("a", {"class": "link", 'type':"text/plain; charset=utf-8"})
 
-    print(urlopen(base_url + download[0]['href']).read().decode('utf-8'))
+    if not os.path.exists(author[0]):
+        os.makedirs(author[0])
+
+    f = open(author[0] + "/" + name.replace(' ', '_') + ".txt", "w+")
+    f.write(urlopen(base_url + download[0]['href']).read().decode('utf-8'))
+    f.close()
 
 if __name__ == '__main__':
     mainloop()
