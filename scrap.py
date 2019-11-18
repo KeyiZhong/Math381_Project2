79d65d703a777f036d88bc3ac0355d82d787f9e4
Keyi Zhong <kz25@uw.edu>
2019-11-17 20:38:04 -0800

    update scrap.py

--- scrap.py
+++ scrap.py
@@ -8,10 +8,10 @@ import fnmatch
 
 search_url = "https://www.gutenberg.org/ebooks/authors/search/?query="
 base_url = "https://www.gutenberg.org"
-author = ['mark twain', 'charles dickens', 'Jane Austen', 'Leo Tolstoy', 'Virginia Woolf', 'F. Scott Fitzgerald',
+authors = ['mark twain', 'charles dickens', 'Jane Austen', 'Leo Tolstoy', 'Virginia Woolf', 'F. Scott Fitzgerald',
           'Lewis Carroll', 'Mary Shelley', 'Herman Melville', 'Oscar Wilde', 'Arthur Conan Doyle']
 
-def downloadPage(books):
+def downloadPage(books, author):
     for i in range(len(books)):
         book_url = books[i].a['href']
 
@@ -19,45 +19,47 @@ def downloadPage(books):
         soup = BeautifulSoup(html, features='lxml')
         name = soup.find('h1').contents[0]
         download = soup.find_all("a", {"class": "link", 'type': re.compile("^text/plain")})
-        if not name.contains("Gutenberg"):
-            if not os.path.exists(author[0]):
-                os.makedirs(author[0])
-
+        if not name.__contains__("Gutenberg"):
             file_exist = False
-            for file in os.listdir(author[0] + "/"):
+            for file in os.listdir(author + "/"):
                 if fnmatch.fnmatch(file, name.replace(' ', '_')[0:10] + "*"):
-                    print(name)
                     file_exist = True
 
             if len(download) != 0 and not file_exist:
                 try:
-                    f = open(author[0] + "/" + name.replace(' ', '_') + ".txt", "w+")
+                    f = open(author + "/" + name.replace(' ', '_') + ".txt", "w+")
                     f.write(urlopen(base_url + download[0]['href']).read().decode('utf-8'))
                     f.close()
                 except:
-                    print(name)
+                    os.remove(author + "/" + name.replace(' ', '_') + ".txt")
+                    print(name + " not download and removed")
 
 
 def mainloop():
     # search author
-    for i in range(len(author)):
-        html = urlopen(search_url + author[i].replace(' ','+')).read().decode('utf-8')
-        soup = BeautifulSoup(html, features='lxml')
-        author_url = soup.find_all("a", {"class": "link", "href": re.compile("^/ebooks/author/")})[0]
+    for i in range(len(authors)):
+        if not os.path.exists(authors[i] + " done"):
+            if not os.path.exists(authors[i]):
+                os.makedirs(authors[i])
+            html = urlopen(search_url + authors[i].replace(' ','+')).read().decode('utf-8')
+            soup = BeautifulSoup(html, features='lxml')
+            author_url = soup.find_all("a", {"class": "link", "href": re.compile("^/ebooks/author/")})[0]
 
-        # select author
-        html = urlopen(base_url + author_url['href']).read().decode('utf-8')
-        soup = BeautifulSoup(html, features='lxml')
-        books = soup.find_all("li", {"class": "booklink"})
-        # download first page
-        downloadPage(books)
-        nextpage = soup.find("a", {'title': 'Go to the next page of results.'})
-        while nextpage:
-            html = urlopen(base_url + nextpage['href']).read().decode('utf-8')
+            # select author
+            html = urlopen(base_url + author_url['href']).read().decode('utf-8')
             soup = BeautifulSoup(html, features='lxml')
             books = soup.find_all("li", {"class": "booklink"})
-            downloadPage(books)
+            # download first page
+            downloadPage(books, authors[i])
             nextpage = soup.find("a", {'title': 'Go to the next page of results.'})
+            while nextpage:
+                html = urlopen(base_url + nextpage['href']).read().decode('utf-8')
+                soup = BeautifulSoup(html, features='lxml')
+                books = soup.find_all("li", {"class": "booklink"})
+                downloadPage(books, authors[i])
+                nextpage = soup.find("a", {'title': 'Go to the next page of results.'})
+            os.rename(authors[i], authors[i] + " done")
+        print("done " + authors[i])
 
 
 if __name__ == '__main__':
